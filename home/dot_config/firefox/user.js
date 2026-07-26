11587d401be498ddb6bbffe2b007ebdd5312d7c7
Jo Panic <github@kubernan.me>
2026-07-26 16:08:48 +0200

    adjust chrome

---
+++ home/dot_config/firefox/user.js
@@ -0,0 +1,5 @@
+// Géré par chezmoi — DPI & UI Firefox / Mullvad Browser
+// DPI auto par écran : correct sur MacBook ET Studio Display
+user_pref("layout.css.devPixelsPerPx", "-1");
+// Active userChrome.css / userContent.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
